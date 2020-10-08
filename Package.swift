ac5a01ec8f0849a5c6da24df5c059a46591dbbd5
Adam Fowler <adamfowler71@gmail.com>
2020-10-08 10:24:02 +0100

    Package is now only Smithy loader, remove codegen code

--- Package.swift
+++ Package.swift
@@ -3,24 +3,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "soto-codegen",
+    name: "soto-smithy",
     products: [
-        .executable(name: "SotoCodeGen", targets: ["SotoCodegen"]),
         .library(name: "SotoSmithy", targets: ["SotoSmithy"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/swift-aws/Stencil.git", .upToNextMajor(from: "0.13.2")),
-    ],
     targets: [
-        .target(
-            name: "SotoCodegen",
-            dependencies: [
-                .byName(name: "SotoSmithy"),
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Stencil", package: "Stencil")]
-            , resources: [.process("Templates")]
-        ),
         .target(name: "SotoSmithy", dependencies: []),
         .testTarget(name: "SotoSmithyTests", dependencies: ["SotoSmithy"]),
     ]
